d582b2cf0a8c936885d3d96198b877cd58135e69
coby <coby5502@gmail.com>
2024-06-28 10:12:56 +0900

    커스텀 픽커 완성

--- CustomTimePicker/ContentView.swift
+++ CustomTimePicker/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var hours: Int = 0
+    @State private var minutes: Int = 30
+    @State private var seconds: Int = 25
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                TimePicker(
+                    hours: self.$hours,
+                    minutes: self.$minutes,
+                    seconds: self.$seconds
+                )
+                .padding(15)
+                .background(.white, in: .rect(cornerRadius: 10))
+                .padding(.horizontal, 20)
+            }
+            .padding(15)
+            .navigationTitle("Custom Time Picker")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.gray.opacity(0.15))
         }
-        .padding()
     }
 }
 
---
+++ CustomTimePicker/View/TimePicker.swift
@@ -0,0 +1,111 @@
+//
+//  TimePicker.swift
+//  CustomTimePicker
+//
+//  Created by Coby Kim on 6/28/24.
+//
+
+import SwiftUI
+
+struct TimePicker: View {
+    
+    var style: AnyShapeStyle = .init(.bar)
+    
+    @Binding var hours: Int
+    @Binding var minutes: Int
+    @Binding var seconds: Int
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            CustomView("hours", range: 0...24, self.$hours)
+            CustomView("mins", range: 0...24, self.$hours)
+            CustomView("secs", range: 0...24, self.$hours)
+        }
+        .offset(x: -25)
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(self.style)
+                .frame(height: 35)
+        }
+    }
+    
+    @ViewBuilder
+    private func CustomView(_ title: String, range: ClosedRange<Int>,
+                            _ selection: Binding<Int>) -> some View {
+        PickerViewWithoutIndicator(selection: self.$hours) {
+            ForEach(range, id: \.self) { value in
+                Text("\(value)")
+                    .frame(width: 35, alignment: .trailing)
+                    .tag(value)
+            }
+        }
+        .overlay {
+            Text(title)
+                .font(.callout)
+                .frame(width: 50, alignment: .leading)
+                .lineLimit(1)
+                .offset(x: 50)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+/// Helpers
+struct PickerViewWithoutIndicator<Content: View, Selection: Hashable>: View {
+    
+    @Binding var selection: Selection
+    @ViewBuilder var content: Content
+    @State private var isHidden: Bool = false
+    
+    var body: some View {
+        Picker("", selection: self.$selection) {
+            if !self.isHidden {
+                RemovePickerIndicator {
+                    self.isHidden = true
+                }
+            } else {
+                self.content
+            }
+        }
+        .pickerStyle(.wheel)
+    }
+}
+
+fileprivate
+struct RemovePickerIndicator: UIViewRepresentable {
+    
+    var result: () -> ()
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        
+        DispatchQueue.main.async {
+            if let pickerView = view.pickerView {
+                if pickerView.subviews.count >= 2 {
+                    pickerView.subviews[1].backgroundColor = .clear
+                }
+                
+                self.result()
+            }
+        }
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
+
+fileprivate
+extension UIView {
+    var pickerView: UIPickerView? {
+        if let view = superview as? UIPickerView {
+            return view
+        }
+        
+        return superview?.pickerView
+    }
+}
