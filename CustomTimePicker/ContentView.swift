daf8df6269db94f84ee3f5e90d46e4c18515fe42
coby <coby5502@gmail.com>
2024-06-28 09:50:21 +0900

    Initial Commit

---
+++ CustomTimePicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTimePicker
+//
+//  Created by Coby Kim on 6/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTimePicker/CustomTimePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTimePickerApp.swift
+//  CustomTimePicker
+//
+//  Created by Coby Kim on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTimePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
